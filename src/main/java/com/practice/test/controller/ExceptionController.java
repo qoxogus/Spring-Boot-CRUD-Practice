2df32a03b47add2eec0b2e1af938f844d9d3a408
qoxogus <a01034367055@gmail.com>
2021-05-24 15:31:05 +0900

    [FIX] delete @RequiredArgsConstructor

--- src/main/java/com/practice/test/controller/ExceptionController.java
+++ src/main/java/com/practice/test/controller/ExceptionController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/exception")
 public class ExceptionController {
